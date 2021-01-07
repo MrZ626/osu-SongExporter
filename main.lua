a96e0afa1380ca0b8c5c3ba7bec46d8ade299c74
MrZ626 <1046101471@qq.com>
2021-01-07 12:26:09 +0800

    修复会读取到导出目录和读取自身lua文件的问题

--- main.lua
+++ main.lua
@@ -6,18 +6,27 @@ repeat
 until io.read():lower()=="y"
 
 local fs=love.filesystem
-local folders=fs.getDirectoryItems("")
+local items=fs.getDirectoryItems("")
 local doneMark={}
 local progressString="[%.1f%%]  %d / %d"
-for count,folderName in next,folders do
-	local songName=folderName:sub(folderName:find(" ")+1)
+local saveDir=fs.getSaveDirectory()
+for i=#items,1,-1 do
+	if fs.getRealDirectory(items[i])==saveDir then
+		fs.remove(items[i])
+		table.remove(items,i)
+	elseif fs.getInfo(items[i]).type=="file"then
+		table.remove(items,i)
+	end
+end
+for count,itemName in next,items do
+	local songName=itemName:sub(itemName:find(" ")+1)
 	if not doneMark[songName]then
 		doneMark[songName]=true
 		local maxsize=sizeThreshold
 		local musicFileName
-		for _,fileName in next,fs.getDirectoryItems(folderName) do
+		for _,fileName in next,fs.getDirectoryItems(itemName) do
 			if fileName:find("mp3")or fileName:find("wav")or fileName:find("ogg")then
-				local info=fs.getInfo(folderName.."/"..fileName)
+				local info=fs.getInfo(itemName.."/"..fileName)
 				if maxsize<info.size then
 					maxsize=info.size
 					musicFileName=fileName
@@ -25,14 +34,14 @@ for count,folderName in next,folders do
 			end
 		end
 		if musicFileName then
-			fs.write(songName..musicFileName:sub(-4),fs.read(folderName.."/"..musicFileName))
+			fs.write(songName..musicFileName:sub(-4),fs.read(itemName.."/"..musicFileName))
 		end
 	end
-	if count%5==0 or count==#folders then
-		print(progressString:format(count/#folders*100,count,#folders))
+	if count%5==0 or count==#items then
+		print(progressString:format(count/#items*100,count,#items))
 	end
 end
-print("\n"..#folders.." Songs exported successfully!")
+print("\n"..#items.." Songs exported successfully!")
 os.execute("pause")
 io.read()
 love.system.openURL(fs.getSaveDirectory())
